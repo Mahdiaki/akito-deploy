2bbaf153822cbe601eba431f63571f71aeab57b2
Mahdiaki <92165123+Mahdiaki@users.noreply.github.com>
2025-09-23 03:36:48 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){ 
   return (    
 <footer className="footer">      
   <div className="container">        
-    Swap land
+    Akito Deploy
  • Built on <strong>ZenChain Testnet</strong>. Faucet:{" "}       
      <a className="pill" href="https://faucet.zenchain.io" target="_blank" rel="noreferrer">Get ZTC</a>      
 </div>    
